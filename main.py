a2880c9aa91465fa31700927498bf55da3b7fd8b
JuanManuelSantamariaArenas <110195136+JuanManuelSantamariaArenas@users.noreply.github.com>
2022-09-13 13:11:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-class Ropa:
+class Usuario:
+
+
+    def __init__(self):
+        pass
+
+
+class Prenda:
+
+
+    def __init__(self):
+        pass
+
+
+class Tienda:
+
 
     def __init__(self):
         pass
